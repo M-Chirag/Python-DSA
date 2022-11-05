acbf385d3f7b99b3a8342d0466bd2d07f8ae5a6c
Chirag Manghani <43926813+M-Chirag@users.noreply.github.com>
2022-11-05 14:19:26 -0700

    Bucket Sort

--- sort.py
+++ sort.py
@@ -56,6 +56,26 @@ class Sorting:
                     arr[j] = temp
         return arr
 
+    def bucketSort(arr):
+        # Assuming arr only contains 0, 1 or 2
+        counts = [0, 0, 0]
+
+    # Count the quantity of each val in arr
+        for n in arr:
+            counts[n] += 1
+
+        # Fill each bucket in the original array
+        i = 0
+        for n in range(len(counts)):
+            for j in range(counts[n]):
+                arr[i] = n
+                i += 1
+        return arr
+
+
+arr = [0, 2, 1, 2, 0]
+print(Sorting.bucketSort(arr))
+
 
 arr = [5, 2, 4, 6, 1, 3, 4]
 arr_strings = ["aa", "a", "a1", "bat", "baa", "ba", "b"]
